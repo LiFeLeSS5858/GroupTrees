199248a16a6a770ebab22414ec12878f544474d4
Burov1234 <62289833+Burov1234@users.noreply.github.com>
2020-07-09 13:13:10 +0300

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -28,7 +28,7 @@ namespace Практика
 
         private void button8_Click(object sender, EventArgs e)
         {
-           
+
         }
 
         void button1_Click(object sender, EventArgs e)
@@ -37,8 +37,66 @@ namespace Практика
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 filename = openFileDialog1.FileName;
-                myfile = File.ReadAllLines(filename);            
+                myfile = File.ReadAllLines(filename);
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] arr;
+            arr = Funck(10);
+        }
+        public static int[] Funck(int N)
+        {
+            int[] Arr = new int[N];
+            for (int j = 0; j < N; j++)
+            {
+                System.Diagnostics.Stopwatch watch;
+                long elapsedMs;
+                watch = System.Diagnostics.Stopwatch.StartNew();
+                var obj = new DictionaryTree();
+                string input_text = System.IO.File.ReadAllText(@"big.txt");
+                string str = "";
+                foreach (var i in input_text)
+                {
+                    if (i >= 'a' && i <= 'z' || i >= 'A' && i <= 'Z' || i == '\'')
+                        str += i;
+                    else if (str.Length > 0)
+                    {
+                        if (obj.ContainsKey(str))
+                            ++obj[str];
+                        else
+                            obj.Add(str, 1);
+                        str = "";
+                    }
+                }
+                watch.Stop();
+                elapsedMs = watch.ElapsedMilliseconds;
+                Arr[j] = (int)elapsedMs;
+
+            }
+            return Arr;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            var obj = new DictionaryTree();
+            string input_text = System.IO.File.ReadAllText(@"big.txt");
+            string str = "";
+            foreach (var i in input_text)
+            {
+                if (i >= 'a' && i <= 'z' || i >= 'A' && i <= 'Z' || i == '\'')
+                    str += i;
+                else if (str.Length > 0)
+                {
+                    if (obj.ContainsKey(str))
+                        ++obj[str];
+                    else
+                        obj.Add(str, 1);
+                    str = "";
+                }
+            }
+            label2.Text = Convert.ToString(obj.InternalNodeCount);
         }
     }
 }
