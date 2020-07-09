39f5da468332594ecbd740d404c23b13630de983
Burov1234 <62289833+Burov1234@users.noreply.github.com>
2020-07-09 13:29:31 +0300

    Revert "Add files via upload"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -42,19 +41,14 @@
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button8 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(16, 430);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Location = new System.Drawing.Point(12, 373);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 27);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load File";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,10 +56,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(16, 14);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(180, 40);
+            this.button2.Size = new System.Drawing.Size(135, 35);
             this.button2.TabIndex = 1;
             this.button2.Text = "Create List Tree";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,78 +66,70 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(16, 61);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Location = new System.Drawing.Point(12, 53);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(180, 40);
+            this.button3.Size = new System.Drawing.Size(135, 35);
             this.button3.TabIndex = 2;
             this.button3.Text = "Create Linked Tree";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(16, 108);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button4.Location = new System.Drawing.Point(12, 94);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(180, 40);
+            this.button4.Size = new System.Drawing.Size(135, 35);
             this.button4.TabIndex = 3;
             this.button4.Text = "Create SortedDictionary Tree";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(16, 156);
-            this.button5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button5.Location = new System.Drawing.Point(12, 135);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(180, 40);
+            this.button5.Size = new System.Drawing.Size(135, 35);
             this.button5.TabIndex = 4;
             this.button5.Text = "Create Dictionary Tree";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(16, 203);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button6.Location = new System.Drawing.Point(12, 176);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(180, 40);
+            this.button6.Size = new System.Drawing.Size(135, 35);
             this.button6.TabIndex = 5;
             this.button6.Text = "Create Node Tree";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(16, 250);
-            this.button7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button7.Location = new System.Drawing.Point(12, 217);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(180, 40);
+            this.button7.Size = new System.Drawing.Size(135, 35);
             this.button7.TabIndex = 6;
             this.button7.Text = "Create HashSet Tree";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // chart1
             // 
-            chartArea3.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
-            this.chart1.Location = new System.Drawing.Point(237, 14);
-            this.chart1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(178, 12);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "ChartArea1";
-            series3.Legend = "Legend1";
-            series3.Name = "Series1";
-            this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(400, 346);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 7;
             this.chart1.Text = "chart1";
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(16, 298);
-            this.button8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button8.Location = new System.Drawing.Point(12, 258);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(180, 40);
+            this.button8.Size = new System.Drawing.Size(135, 35);
             this.button8.TabIndex = 8;
             this.button8.Text = "Create Graphic";
             this.button8.UseVisualStyleBackColor = true;
@@ -153,49 +138,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(233, 430);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(175, 373);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 9;
             this.label1.Text = "label1";
             // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(0, 0);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(75, 23);
-            this.button9.TabIndex = 10;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(457, 366);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(180, 36);
-            this.button10.TabIndex = 11;
-            this.button10.Text = "InternalNodeCount";
-            this.button10.UseVisualStyleBackColor = true;
-            this.button10.Click += new System.EventHandler(this.button10_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(522, 415);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 15);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "label2";
-            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 474);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button10);
-            this.Controls.Add(this.button9);
+            this.ClientSize = new System.Drawing.Size(515, 411);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.chart1);
@@ -206,7 +159,6 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
@@ -227,10 +179,6 @@
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Label label1;
-        private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Label label2;
     }
 }
 
